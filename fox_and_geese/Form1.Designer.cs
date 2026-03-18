c4fc96f465344c4df9f055feb883991442158630
Alisa Arushanian <alisa.arus@gmail.com>
2026-03-18 13:21:28 +0300

    initial commit. start the project

---
+++ fox_and_geese/Form1.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace fox_and_geese
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 777);
+            this.MaximumSize = new System.Drawing.Size(816, 816);
+            this.MinimumSize = new System.Drawing.Size(816, 816);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Лиса и гуси";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
