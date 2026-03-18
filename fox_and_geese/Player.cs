1a1071ce1e7a1c59d0d7775829eef1bcbef95e75
Alisa Arushanian <alisa.arus@gmail.com>
2026-03-18 14:26:27 +0300

    add player abstract class

--- fox_and_geese/Player.cs
+++ fox_and_geese/Player.cs
@@ -8,6 +8,15 @@ namespace fox_and_geese
 {
     public abstract class Player
     {
+        private Position current_position;
+        private List<Position> possible_moves;
 
+        public Position Current_position { get => current_position; set => current_position = value; }
+        public List<Position> Possible_moves { get => possible_moves; set => possible_moves = value; }
+        protected Player(Position cur_pos, List<Position> pos_mov)
+        {
+            this.current_position = cur_pos;
+            this.possible_moves = pos_mov;
+        }
     }
 }
